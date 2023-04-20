5c42b2d3315b9e237d59b1e512f4ccfd9edcd90c
Carlos Eduardo <carlao0010@hotmail.com>
2023-04-19 21:25:30 -0300

    Requisito 4 & CSS

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -4,21 +4,16 @@ import Button from '../components/Button';
 class Products extends React.Component {
   render() {
     return (
-      <>
-        <label
-          data-testid="home-initial-message"
-          htmlFor="product"
-        >
-          Digite algum termo de pesquisa ou escolha uma categoria.
-          <input
-            id="product"
-            name="product"
-            type="text"
-            placeholder="Digite o produto"
-          />
-        </label>
+      <div className="product-container">
+        <input
+          id="product"
+          name="product"
+          type="text"
+          placeholder="🔍 Digite o produto"
+        />
+        <h3>Digite algum termo de pesquisa ou escolha uma categoria.</h3>
         <Button />
-      </>
+      </div>
 
     );
   }
