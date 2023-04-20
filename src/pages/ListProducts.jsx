cb06af14a815614f3286c41b1300f9faa3f66c44
Carlos Eduardo <carlao0010@hotmail.com>
2023-04-20 13:53:04 -0300

    Iniciando req5

--- src/pages/ListProducts.jsx
+++ src/pages/ListProducts.jsx
@@ -4,30 +4,35 @@ import Button from '../components/Button';
 
 // api.getCategories().then((categories) => { console.log(categories); });
 
-// api.getProductsFromCategoryAndQuery('carro').then((categories) => {
-//   console.log(categories);
-// });
+api.getProductsFromCategoryAndQuery('acessórios').then((categories) => {
+  console.log(categories);
+});
 
-// api.getProductsFromCategoryAndQuery('MLB2203652659').then((categories) => {
-//   console.log(categories);
-// });
+api.getProductsFromCategoryAndQuery('MLB5672').then((categories) => {
+  console.log(categories);
+});
 
 const INDEX_NOT_FOUND = -1;
 
-class Products extends React.Component {
+class ListProducts extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
       handleCategories: [],
       selectedCategoryId: [],
+      categoriesSelected: [],
+      inputQuery: '',
     };
 
     this.handleInputCheckbox = this.handleInputCheckbox.bind(this);
+    this.handleInputQuery = this.handleInputQuery.bind(this);
+    this.searchProducts = this.searchProducts.bind(this);
   }
 
   componentDidMount() {
     this.handleFetchCategory();
+    this.searchProducts();
   }
 
   handleFetchCategory = async () => {
@@ -35,6 +40,8 @@ class Products extends React.Component {
 
     this.setState({ handleCategories: [...categories] });
   };
+
+  //! CRIA O BOTÃOOOOOOOO
 
   handleInputCheckbox({ target }) {
     const { value } = target;
@@ -59,8 +66,44 @@ class Products extends React.Component {
     });
   }
 
+  handleInputQuery({ target }) {
+    const { value } = target;
+
+    this.setState({ inputQuery: value });
+  }
+
+  searchProducts = async () => {
+    const { selectedCategoryId, inputQuery } = this.state;
+
+    let categories = [];
+
+    if (selectedCategoryId.length > 0) {
+      const promises = selectedCategoryId
+        .map((id) => api.getProductsFromCategoryAndQuery(id));
+
+      const results = await Promise.all(promises);
+
+      categories = results.reduce((acc, result) => acc.concat(result), []);
+    }
+
+    if (inputQuery) {
+      const result = await api.getProductsFromCategoryAndQuery(inputQuery);
+      categories = [...result];
+    }
+
+    this.setState({ categoriesSelected: categories });
+    console.log(categories);
+  };
+
   render() {
-    const { handleCategories } = this.state;
+    const {
+      handleCategories,
+      selectedCategoryId,
+      categoriesSelected,
+      inputQuery,
+    } = this.state;
+    console.log(categoriesSelected);
+
     return (
       <div className="container">
         <div className="categories-container">
@@ -87,17 +130,27 @@ class Products extends React.Component {
             name="input-query"
             type="text"
             placeholder="🔍 Digite o produto"
+            value={ inputQuery }
+            onChange={ this.handleInputQuery }
           />
-          <h3
-            data-testid="home-initial-message"
-          >
-            Digite algum termo de pesquisa ou escolha uma categoria.
-          </h3>
-          <Button />
+          {selectedCategoryId.length === 0
+            ? (
+              <div>
+                <h3
+                  data-testid="home-initial-message"
+                >
+                  Digite algum termo de pesquisa ou escolha uma categoria.
+                </h3>
+                <Button />
+              </div>
+            )
+            : (
+              <p>resultado</p>
+            )}
         </div>
       </div>
     );
   }
 }
 
-export default Products;
+export default ListProducts;
