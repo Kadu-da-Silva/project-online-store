46a856abad71c4807f5ba70c9eaecf35db02ce27
Carlos Eduardo <carlao0010@hotmail.com>
2023-04-25 15:18:03 -0300

    Requisito 08

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { getItem } from '../services/localStorageFuncs';
 
 class Cart extends React.Component {
+  state = {
+    products: getItem('products') || '',
+    // counter: getItem('counter'),
+  };
+
   render() {
+    const { products, counter } = this.state;
+    console.log(products);
     return (
-      <div data-testid="shopping-cart-empty-message">Seu carrinho está vazio</div>
+      <div data-testid="shopping-cart-empty-message">
+        {products.length > 0
+          ? (
+            <div>
+              {products.map((element) => (
+                <div key={ element.id }>
+                  <h2 data-testid="shopping-cart-product-name">{ element.title }</h2>
+                  <img src={ element.thumbnail } alt={ element.title } />
+                  <p>{counter}</p>
+                  <p>{ element.price }</p>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <p>Carrinho vazio</p>
+          )}
+      </div>
 
     );
   }
